a49ad1ab2085157a30bc9b4a5f2923a3f13b9dfd
DJDEVJAMS <alex.art.brown@gmail.com>
2024-07-27 15:39:37 -0500

    module install and index edit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const render = require (lib/shapes.js);
 
 
 function createSVG() {
@@ -16,31 +17,44 @@ name: "text",
 // THEN I can enter a color keyword (OR a hexadecimal number)
     {
 type: "input",
-message: "Enter a color keyword (ora hexadecimal number) for text color",
+message: colors.brightblue("Enter a color keyword (ora hexadecimal number) for text color"),
 name: "text_color",
            },
- //    WHEN I am prompted for a shape
- //    THEN I am presented with a list of shapes to choose from: circle, triangle, and square     
+ // WHEN I am prompted for a shape
+ // THEN I am presented with a list of shapes to choose from: circle, triangle, and square     
  {
 type: 'list',
-message: colors.brightMagenta('What licensing did you use?'),
-name: 'licensing',
+message: ('Pick a shape for your logo.'),
+name: 'shape',
 choices: ['circle', 'triangle', 'square']
 },
 // WHEN I am prompted for the shape's color
 // THEN I can enter a color keyword (OR a hexadecimal number)
 {
 type: "input",
-message: "Enter a color keyword (ora hexadecimal number) for shape color",
+message: colors.brightblue ("Enter a color keyword (ora hexadecimal number) for shape color"),
 name: "shape_color",
-       },
+},
     
 ] .then((data) => {
+    const make_shape = render(data.shape)
     const logo_render = 
 
 // Logo build
 
+`<svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+  <polygon points="150, 18 244, 182 56, 182" fill="${data.shape_color}" />
 
-})
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.text_color}">${date.text}</text>
+
+</svg>`;
+
+fs.writeFile('/examples/log.svg', readmeContent, (err) =>
+    err ? console.error(err) : console.log('logo.svg created successfully!')
+);
+}};
 }
-}
+
+createSVG()
